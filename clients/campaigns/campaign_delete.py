d54deba0c2f817abb1a794577ae2d5563ddaea8e
rogelio_ensch <Pokemon2000>
2018-11-13 17:59:43 -0600

    actualización delete

--- clients/campaigns/campaign_delete.py
+++ clients/campaigns/campaign_delete.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import unittest
 import time
 
@@ -10,11 +11,25 @@ from util.functions import login
 from util.config import ModelConfig
 from util.functions import screenshot, logout
 
+campaign='''
+    [{"name" : "RogelioElim","budget":"2.00", "url":"https://www.google.com","objetive":"2","industry":"Automotriz","category":"llantas","camcode":"ENSCH-75"},
+    {"name" : "Rogelio 2","budget":"4.0","url":"https://www.facebook.com/a","objetive":"4.0"}]'''
+
 
 class AddClient(unittest.TestCase):
     def setUp(self):
+        global campaign
         self.driver = ModelConfig.driver_web
         self.driver.maximize_window()
+        info = json.loads(campaign)
+        code = """
+info = {0}
+cur.execute("DELETE FROM campaigns WHERE name = '%s' AND budget = '%s' AND objetive = '%s'" %(info[0]['name'], info[0]['budget'], info[0]['objetive']))
+sql = 'INSERT INTO campaigns (url, cam_code, name, budget, objetive, industry, category, created_at, updated_at, redirect_url, script_snippet, status, ga_api_key,ga_api_secret,dbm_client_secret,dbm_client_id, client_id) VALUES (%s,%s, %s, %s, %s, %s, %s, %s,%s,%s, %s, %s,%s,%s,%s,%s,%s)'
+val = (info[0]['url'], info[0]['camcode'],info[0]['name'], info[0]['budget'], info[0]['objetive'],info[0]['industry'], info[0]['category'], strftime("%Y/%m/%d"), strftime("%Y/%m/%d"),'','',1,'','','','',2)
+cur.execute(sql, val)
+""".format(info)
+        db_functions(code)
 
 
     def testAddClient(self):
