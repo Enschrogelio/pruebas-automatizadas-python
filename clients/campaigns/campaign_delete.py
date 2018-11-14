5c4265bb3a3f51d22d7368a48f41f36b0d5529a1
rogelio_ensch <Pokemon2000>
2018-11-14 12:23:17 -0600

    correcciones de código

--- clients/campaigns/campaign_delete.py
+++ clients/campaigns/campaign_delete.py
@@ -5,74 +5,54 @@ import time
 
 
 from util.functions import *
-from util.dataCampaign import *
 from util.functions import login
-
 from util.config import ModelConfig
 from util.functions import screenshot, logout
 
 campaign='''
-    [{"name" : "RogelioElim","budget":"2.00", "url":"https://www.google.com","objetive":"2","industry":"Automotriz","category":"llantas","camcode":"ENSCH-75"},
+    [{"name" : "Rogelio 2","budget":"4.00", "url":"https://www.google.com","objetive":"4","industry":"Automotriz","category":"llantas","camcode":"ENSCH-75"},
     {"name" : "Rogelio 2","budget":"4.0","url":"https://www.facebook.com/a","objetive":"4.0"}]'''
-
+valor=""
 
 class AddClient(unittest.TestCase):
     def setUp(self):
-        global campaign
+        global campaign, valor
         self.driver = ModelConfig.driver_web
         self.driver.maximize_window()
         info = json.loads(campaign)
         code = """
 info = {0}
 cur.execute("DELETE FROM campaigns WHERE name = '%s' AND budget = '%s' AND objetive = '%s'" %(info[0]['name'], info[0]['budget'], info[0]['objetive']))
-sql = 'INSERT INTO campaigns (url, cam_code, name, budget, objetive, industry, category, created_at, updated_at, redirect_url, script_snippet, status, ga_api_key,ga_api_secret,dbm_client_secret,dbm_client_id, client_id) VALUES (%s,%s, %s, %s, %s, %s, %s, %s,%s,%s, %s, %s,%s,%s,%s,%s,%s)'
+sql = 'INSERT INTO campaigns (url, cam_code, name, budget, objetive, industry, category, created_at, updated_at, redirect_url, script_snippet, status, ga_api_key,ga_api_secret,dbm_client_secret,dbm_client_id, client_id) VALUES (%s,%s, %s, %s, %s, %s, %s, %s,%s,%s, %s, %s,%s,%s,%s,%s,%s) returning name, budget, objetive'
 val = (info[0]['url'], info[0]['camcode'],info[0]['name'], info[0]['budget'], info[0]['objetive'],info[0]['industry'], info[0]['category'], strftime("%Y/%m/%d"), strftime("%Y/%m/%d"),'','',1,'','','','',2)
 cur.execute(sql, val)
 """.format(info)
-        db_functions(code)
+        valor = db_functions(code)[0]
+        #print(valor)
 
 
     def testAddClient(self):
+        global campaign, valor
+        info = json.loads(campaign)
         driver = self.driver
         #login
         login(self)
-        time.sleep(2)
+        sleep(2)
         #Click en clientes
         driver.find_element_by_xpath('//*[@id="clienttable"]/tbody/tr[1]/td[6]/a[1]/i').click()
-        time.sleep(1)
-        #Click en view
-        driver.find_element_by_xpath('//*[@id="client-camp-header"]/div/button').click()
-        time.sleep(1)
-        #llenado de Form
-        #name
-        driver.find_element_by_xpath('//*[@id="form-add-campaign"]/div[1]/input').send_keys("dsasd")
-        time.sleep(2)
-        #Seleccionar Select contenedor ACTIVE
-        driver.find_element_by_xpath('//*[@id="form-add-campaign"]/div[2]').click()
-        time.sleep(2)
-        #Seleccionar Active
-        driver.find_element_by_xpath('//*[@id="form-add-campaign"]/div[2]/select/option[1]').click()
-        time.sleep(2)
-        #Seleccionar Select contenedor INDUSTRIA
-        driver.find_element_by_xpath('//*[@id="form-add-campaign"]/div[3]').click()
-        time.sleep(2)
-        #Seleccionar AUTOMOTRIZ
-        driver.find_element_by_xpath('//*[@id="form-add-campaign"]/div[3]/select/option[6]').click()
-        time.sleep(2)
-        #Seleccionar Select contenedor CATEGORY
-        driver.find_element_by_xpath('//*[@id="form-add-campaign"]/div[4]').click()
-        time.sleep(2)
-        #Seleccionar CELULARES
-        driver.find_element_by_xpath('//*[@id="form-add-campaign"]/div[4]/select/option[6]').click()
-        #BUGET
-        driver.find_element_by_xpath('//*[@id="form-add-campaign"]/div[5]/input').send_keys(2)
-        #URL
-        driver.find_element_by_xpath('//*[@id="form-add-campaign"]/div[6]/input').send_keys("https://www.g.com")
-        #OBJETIVE
-        driver.find_element_by_xpath('//*[@id="form-add-campaign"]/div[7]/input').send_keys(100)
-        #enter
-        driver.find_element_by_xpath("//div[10]/div[1]/div[1]/div[3]/button[1]").click()
+        sleep(2)
+        self.assertEqual(info[0]['name'], driver.find_element_by_xpath('//*[@id="campaigntable"]/tbody/tr/td[@title="%s"]' %valor[0]).get_attribute("innerText").replace("\t",""), msg=None)
+        self.assertEqual("%s"%valor[1], driver.find_element_by_xpath('//tr[1]/td[6]').get_attribute("innerText").replace("\t",""), msg=None)
+        self.assertEqual("%s"%valor[2], driver.find_element_by_xpath('//tr[1]/td[7]').get_attribute("innerText").replace("\t",""), msg=None)
+        sleep(3)
+        driver.find_element_by_xpath("//tr[1]/td[10]/a[@class='to-delete' and 3]/i[@class='glyphicon glyphicon-trash' and 1]").click()
         mi_ruta="clients/campaigns/testCampaign/screenshot/"
+        sleep(2)
+        #message "Deleting record"
+        driver.find_element_by_xpath("//button[@class='btn-green text-uppercase col-sm-12']").click()
+        #message confirmaion
+        driver.find_element_by_xpath("//input[@id='input-email']").send_keys("%s"%valor[0])
+        sleep(2)
         screenshot(self,mi_ruta)
         logout(self)
 
--- util/functions.py
+++ util/functions.py
@@ -3,17 +3,19 @@ import random
 import string
 
 import psycopg2
-from pruebas_automatizadas.util.config import modelConfig
+from util.config import ModelConfig
 from time import *
 
 
 
 def db_functions(code):
     conn = None
+    valor=""
     try:
-        conn = psycopg2.connect(modelConfig.connection)
+        conn = psycopg2.connect(ModelConfig.connection)
         cur = conn.cursor()
         exec(code)
+        valor = cur.fetchmany()
         conn.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -21,19 +23,20 @@ def db_functions(code):
     finally:
         if conn is not None:
             conn.close()
+            return valor
 
 def login(self):
     driver = self.driver
     #login
-    driver.get(modelConfig.urlLogin)
-    driver.find_element_by_xpath('//*[@id="id_username"]').send_keys(modelConfig.email)
-    driver.find_element_by_xpath('//*[@id="id_password"]').send_keys(modelConfig.password)
+    driver.get(ModelConfig.url_login)
+    driver.find_element_by_xpath('//*[@id="id_username"]').send_keys(ModelConfig.email)
+    driver.find_element_by_xpath('//*[@id="id_password"]').send_keys(ModelConfig.password)
     driver.find_element_by_xpath('//*[@id="formLogin"]/button').click()
 
 def logout(self):
     sleep(5)
     driver = self.driver
-    driver.get(modelConfig.baseUrl+"/admin/login")
+    driver.get(ModelConfig.base_url+"/admin/login")
     sleep(1)
     driver.find_element_by_xpath('//a[@href="/admin/logout/"]').click()
     sleep(2)
@@ -47,7 +50,7 @@ def screenshot(self,ruta):
     min = now.min
     second = now.second
     today = datetime.date.today()
-    driver.save_screenshot(modelConfig.base_screenshot+ruta+"%s-hora-%s-seg_%s.png" %(today, hour, second))
+    driver.save_screenshot(ModelConfig.base_screenshot+ruta+"%s-hora-%s-seg_%s.png" %(today, hour, second))
     return ruta
 
 def randoms(long, tipo):
