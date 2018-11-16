63e3c3bd67cf96fa071682096852b648e55c7906
Cesar_Perez <cesar.perez@varangard.com>
2018-11-16 16:50:22 -0600

    Modificaciones

--- util/config.py
+++ util/config.py
@@ -4,8 +4,8 @@ from selenium import webdriver
 root_files = os.path.dirname(os.path.abspath(__file__))+'\\'
 base_driver = root_files+"drivers/"
 
-class modelConfig:
-    #atributos
+class ModelConfig:
+    # atributos
     base_url = "http://stage.eupam5k9mb.us-west-2.elasticbeanstalk.com"
     url_login = base_url+"/admin/login/"
     email = "admin@admin.com"
@@ -15,10 +15,10 @@ class modelConfig:
                  " host='cerebro-stage.ct2o8jgma4vs.us-west-2.rds.amazonaws.com' password='N23E4Jz8KLRuSvGb'" \
                  " connect_timeout=10 "
 
-    #Screenshot
+    # Screenshot
     base_screenshot = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'\\'
 
-    #Drivers
+    # Drivers
     def drivers(x):
         global root_driver
         if x == "ch":
