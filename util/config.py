bfebed8108e20717c4bcca55c97e91d8cbf3efa1
Cesar_Perez <cesar.perez@varangard.com>
2018-11-16 16:58:49 -0600

    Modificaciones

--- util/config.py
+++ util/config.py
@@ -4,16 +4,19 @@ from selenium import webdriver
 root_files = os.path.dirname(os.path.abspath(__file__))+'\\'
 base_driver = root_files+"drivers/"
 
+
 class ModelConfig:
-    # atributos
+
+    # Attributes
     base_url = "http://stage.eupam5k9mb.us-west-2.elasticbeanstalk.com"
-    url_login = base_url+"/admin/login/"
+    url_login = base_url + "/admin/login/"
     email = "admin@admin.com"
     password = "admin"
-    connection = "dbname='cerebro'" \
-                 " user='cerebro'" \
-                 " host='cerebro-stage.ct2o8jgma4vs.us-west-2.rds.amazonaws.com' password='N23E4Jz8KLRuSvGb'" \
-                 " connect_timeout=10 "
+    connection = "dbname='cerebro' " \
+                 "user='cerebro' " \
+                 "host='cerebro-stage.ct2o8jgma4vs.us-west-2.rds.amazonaws.com' " \
+                 "password='N23E4Jz8KLRuSvGb' " \
+                 "connect_timeout=10 "
 
     # Screenshot
     base_screenshot = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'\\'
