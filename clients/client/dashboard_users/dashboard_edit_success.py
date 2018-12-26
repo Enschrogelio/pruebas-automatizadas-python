221ab9e4178eaf29281a59b5f9bc505712b431d8
CesarPR <cesar.perez@varangard.com>
2018-12-26 14:03:41 -0600

    validaciones

---
+++ clients/client/dashboard_users/dashboard_edit_success.py
@@ -0,0 +1,86 @@
+import json
+import unittest
+from time import sleep
+from util.functions import db_functions, login, logout
+from util.config import ModelConfig
+
+# Variables
+client = "arcapruebas2@gmail.com"
+dashboard_success = '''
+    [
+        {"name" : "andres", "email" : "pacheco_mendez1@gmail.com", "password" : "mendez123456", "status": 1},
+        {"name" : "juan", "email" : "pedraza_hernandez1@gmail.com", "password" : "1a23456789", "status": 2}
+    ]
+    '''
+
+
+class AddCampaign(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = ModelConfig.driver_web
+
+    def testuserempty(self):
+        driver = self.driver
+        info = json.loads(dashboard_success)
+        code = """
+info = {0}
+client = "{1}"
+for d_user in info:
+    cur.execute("SELECT du.user_id FROM dashboard_users AS du JOIN users ON du.user_id = users.id where name = '%s' "  
+                "and email = '%s'" % (d_user['name'], d_user['email']))
+    try:
+        id = cur.fetchone()[0]
+        cur.execute("DELETE FROM dashboard_users WHERE user_id=%s" % id)
+        cur.execute("DELETE FROM admin_historicaluser WHERE history_user_id = %s" % id)
+        cur.execute("DELETE FROM users WHERE id = %s" % id)
+    except Exception as errorException:
+        errorException
+cur.execute("INSERT INTO users (name, password, status, email, created_at, updated_at, is_active, is_client) VALUES "
+            "('%s', '%s', 1, '%s', current_timestamp, current_timestamp, true, true) RETURNING id;" % (info[1]['name'], 
+            info[1]['password'], info[1]['email']))
+id = cur.fetchone()[0]
+
+cur.execute("SELECT id FROM clients WHERE email = '%s'" % client)
+id_client = cur.fetchone()[0]
+cur.execute("INSERT INTO dashboard_users (client_id, user_id) VALUES (%s, %s)" % (id_client, id))
+""".format(info, client)
+        db_functions(code)
+        login(self)
+        driver.find_element_by_xpath('//*[@id="inputSrc"]').click()
+        sleep(1)
+        driver.find_element_by_xpath('//*[@id="search"]').send_keys(client)
+        sleep(1)
+        # view
+        driver.find_element_by_xpath('//*[@id="clienttable"]/tbody/tr[1]/td[5]/a[1]').click()
+        sleep(3)
+        # button_dashboard
+        driver.find_element_by_xpath('//*[@id="user-dashboard"]/div/div/div[4]/a[1]').click()
+        sleep(3)
+        # email
+        driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]').clear()
+        driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]').send_keys((info[0]["email"]))
+        # name
+        driver.find_element_by_xpath('//*[@id="edit-dash-user-name"]').clear()
+        driver.find_element_by_xpath('//*[@id="edit-dash-user-name"]').send_keys((info[0]["name"]))
+        sleep(3)
+        # password
+        driver.find_element_by_xpath('//*[@id="edit-dash-user-status"]').click()
+        driver.find_element_by_xpath('//*[@id="edit-dash-user-status"]').send_keys((info[0]["status"]))
+        # confirmation de password
+        # save
+        driver.find_element_by_xpath('//*[@id="modal-edit-dashboard-user"]/div/div/div[3]/button').click()
+        sleep(3)
+        # asser
+        self.assertEqual(info[0]["name"], driver.find_element_by_xpath('//*[@id="user-dashboard"]/div/div/div[2]')
+                         .text, msg=None)
+        self.assertEqual(info[0]["email"], driver.find_element_by_xpath('//*[@id="user-dashboard"]/div/div/div[1]')
+                         .text, msg=None)
+
+    def tearDown(self):
+        logout(self)
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
+
--- clients/client/dashboard_users/dashboard_edit_validation.py
+++ clients/client/dashboard_users/dashboard_edit_validation.py
@@ -1,17 +1,19 @@
 from time import sleep
+import re
 import json
 import unittest
 from util.config import ModelConfig
-from util.functions import login, logout, randoms
+from util.functions import login, logout, randoms, db_functions
 
 # Variables
 client = "arcapruebas2@gmail.com"
 dashboard_user = '''
     [
         {"name" : "andres", "email":"pacheco_mendez1@gmail.com", "password" : "mendez123456"},
-        {"name" : "juan", "email":"pedraza_hernandez1@gmail.com", "password" : "mendez123456"},
+        {"name" : "juan", "email":"pedraza_hernandez1@gmail.com", "password" : "1a23456789"}
     ]
     '''
+
 
 # noinspection PyCallByClass,PyTypeChecker,PyUnresolvedReferences
 class AddCampaign(unittest.TestCase):
@@ -22,7 +24,7 @@ class AddCampaign(unittest.TestCase):
         driver.find_element_by_xpath('//*[@id="inputSrc"]').click()
         sleep(1)
         driver.find_element_by_xpath('//*[@id="search"]').send_keys(client)
-        sleep(1)
+        sleep(3)
         # view
         driver.find_element_by_xpath('//*[@id="clienttable"]/tbody/tr[1]/td[5]/a[1]').click()
         sleep(3)
@@ -35,17 +37,26 @@ class AddCampaign(unittest.TestCase):
         info = json.loads(dashboard_user)
         code = """
 info = {0}
-cur.execute("SELECT du.user_id FROM dashboard_users AS du JOIN users ON du.user_id = users.id where name = '%s' "  
-            "and email = '%s'" % (info[0]['name'], info[0]['email']))
-try:
-    id = cur.fetchone()[0]
-    cur.execute("DELETE FROM dashboard_users WHERE user_id=%s" % id)
-    cur.execute("DELETE FROM admin_historicaluser WHERE history_user_id = %s" % id)
-    cur.execute("DELETE FROM users WHERE id = %s" % id)
-except Exception as errorException:
-    errorException
+client = "{client}"
+for d_user in info:
+    cur.execute("SELECT du.user_id FROM dashboard_users AS du JOIN users ON du.user_id = users.id where name = '%s' "  
+                "and email = '%s'" % (d_user['name'], d_user['email']))
+    try:
+        id = cur.fetchone()[0]
+        cur.execute("DELETE FROM dashboard_users WHERE user_id=%s" % id)
+        cur.execute("DELETE FROM admin_historicaluser WHERE history_user_id = %s" % id)
+        cur.execute("DELETE FROM users WHERE id = %s" % id)
+    except Exception as errorException:
+        errorException
+cur.execute("INSERT INTO users (name, password, status, email, created_at, updated_at, is_active, is_client) VALUES "
+            "('%s', '%s', 1, '%s', current_timestamp, current_timestamp, true, true) RETURNING id;" % (info[0]['name'], 
+            info[0]['password'], info[0]['email']))
+id = cur.fetchone()[0]
 
-""".format(info)
+cur.execute("SELECT id FROM clients WHERE email = '%s'" % client)
+id_client = cur.fetchone()[0]
+cur.execute("INSERT INTO dashboard_users (client_id, user_id) VALUES (%s, %s)" % (id_client, id))
+""".format(info, client=client)
         db_functions(code)
         cls.driver = ModelConfig.driver_web
         cls.dashboard_main(cls)
@@ -55,29 +66,43 @@ except Exception as errorException:
         sleep(2)
         # email
         driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]').clear()
-        driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]') \
-            .send_keys(randoms(10, "alpha"))
-        # name
-        driver.find_element_by_xpath('//*[@id="edit-dash-user-name"]').clear()
-        driver.find_element_by_xpath('//*[@id="edit-dash-user-name"]')\
-            .send_keys(randoms(10, "alpha"))
-        # active
-        driver.find_element_by_xpath('//*[@id="edit-dash-user-status"]').click()
-        # save
+        email_format = re.compile(r'\b[\w.%+-]+@([\w.-]{1,64})+\.[a-zA-Z]{2,64}\b')
+        email = "%s@%s.%s" % (randoms(3, "alpha"), randoms(4, "alpha"), randoms(1, "alpha"))
+        self.assertFalse(email_format.match(email), msg=None)
+        driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]').send_keys(email)
         driver.find_element_by_xpath('//*[@id="modal-edit-dashboard-user"]/div/div/div[3]/button').click()
+        sleep(1)
+        self.assertEqual(driver.find_element_by_xpath('//*[@id="form-edit-dashboard-user"]/div[1]/span')
+                         .get_attribute('innerHTML'), "Enter a valid email address.", msg=None)
 
     def test_edit_max(self):
         driver = self.driver
+        sleep(2)
         # email
         driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]').clear()
-        driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]') \
-            .send_keys(randoms(100, "alpha"))
-        # name
+        driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]').send_keys(randoms(260, "alpha"))
+        self.assertEqual(len(driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]').get_attribute("value")),
+                         255, msg=None)
+        email_format = re.compile(r'\b[\w.%+-]+(@[\w.-]{1,64})+\.[a-zA-Z]{2,64}\b')
+        # email platform
+        email = "%s@%s.%s" % (randoms(3, "alpha"), randoms(65, "alpha"), randoms(2, "alpha"))
+        self.assertFalse(email_format.match(email), msg=None)
+        driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]').send_keys(email)
+        driver.find_element_by_xpath('//*[@id="modal-edit-dashboard-user"]/div/div/div[3]/button').click()
+        sleep(1)
+        self.assertEqual(driver.find_element_by_xpath('//*[@id="form-edit-dashboard-user"]/div[1]/span')
+                         .get_attribute('innerHTML'), "Enter a valid mail", msg=None)
+        # email extension
+        email = "%s@%s.%s" % (randoms(3, "alpha"), randoms(2, "alpha"), randoms(65, "alpha"))
+        self.assertFalse(email_format.match(email), msg=None)
+        driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]').send_keys(email)
+        driver.find_element_by_xpath('//*[@id="modal-edit-dashboard-user"]/div/div/div[3]/button').click()
+        sleep(1)
+        self.assertEqual(driver.find_element_by_xpath('//*[@id="form-edit-dashboard-user"]/div[1]/span')
+                         .get_attribute('innerHTML'), "Enter a valid mail", msg=None)
+
         driver.find_element_by_xpath('//*[@id="edit-dash-user-name"]').clear()
-        driver.find_element_by_xpath('//*[@id="edit-dash-user-name"]') \
-            .send_keys(randoms(100, "alpha"))
-        # active
-        driver.find_element_by_xpath('//*[@id="edit-dash-user-status"]').click()
+        driver.find_element_by_xpath('//*[@id="edit-dash-user-name"]').send_keys(randoms(100, "alpha"))
         # save
         driver.find_element_by_xpath('//*[@id="modal-edit-dashboard-user"]/div/div/div[3]/button').click()
 
@@ -85,17 +110,11 @@ except Exception as errorException:
         driver = self.driver
         # email
         driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]').clear()
-        driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]') \
-            .send_keys(randoms(100, "special"))
-        sleep(3)
-        # name
-        driver.find_element_by_xpath('//*[@id="edit-dash-user-name"]').clear()
-        driver.find_element_by_xpath('//*[@id="edit-dash-user-name"]') \
-            .send_keys(randoms(100, "special"))
-        # active
-        driver.find_element_by_xpath('//*[@id="edit-dash-user-status"]').click()
-        # save
+        driver.find_element_by_xpath('//*[@id="edit-dash-user-email"]').send_keys(randoms(100, "special"))
         driver.find_element_by_xpath('//*[@id="modal-edit-dashboard-user"]/div/div/div[3]/button').click()
+        sleep(1)
+        self.assertEqual(driver.find_element_by_xpath('//*[@id="form-edit-dashboard-user"]/div[1]/span')
+                         .get_attribute('innerHTML'), "Enter a valid mail", msg=None)
 
     @classmethod
     def tearDownClass(cls):
