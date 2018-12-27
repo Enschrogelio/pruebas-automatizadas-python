7eba1cfce498d8098817caea6e8feeb713ea3744
CesarPR <cesar.perez@varangard.com>
2018-12-27 14:02:30 -0600

    Merge remote-tracking branch 'remotes/origin/master' into campaign
    # Conflicts:
    #	.idea/workspace.xml
    #	util/config.py
    #	util/functions.py

--- util/config.py
+++ util/config.py
@@ -2,40 +2,41 @@ import os
 from selenium import webdriver
 
 root_files = os.path.dirname(os.path.abspath(__file__))+'\\'
-root_driver = root_files+"drivers/"
+base_driver = root_files+"drivers/"
+
 
 class ModelConfig:
-    #atributos
+
+    # Attributes
     base_url = "http://stage.eupam5k9mb.us-west-2.elasticbeanstalk.com"
-    url_login = base_url+"/admin/login/"
+    url_login = base_url + "/admin/login/"
     email = "admin@admin.com"
     password = "admin"
-    connection = "dbname='cerebro'" \
-                 " user='cerebro'" \
-                 " host='cerebro-stage.ct2o8jgma4vs.us-west-2.rds.amazonaws.com' password='N23E4Jz8KLRuSvGb'" \
-                 " connect_timeout=10 "
+    connection = "dbname='cerebro' " \
+                 "user='cerebro' " \
+                 "host='cerebro-stage.ct2o8jgma4vs.us-west-2.rds.amazonaws.com' " \
+                 "password='N23E4Jz8KLRuSvGb' " \
+                 "connect_timeout=10 "
 
-    #screenshot
+    # Screenshot
     base_screenshot = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'\\'
 
-    #Drivers
+    # Drivers
     def drivers(x):
         global root_driver
         if x == "ch":
-            driver = webdriver.Chrome(executable_path=root_driver+"chromedriver.exe")
+            driver = webdriver.Chrome(base_driver+"chromedriver.exe")
+            return driver
+        elif x == "fi":
+            driver = webdriver.Firefox(executable_path=base_driver+"geckodriver.exe")
+            return driver
+        elif x == "ie":
+            driver = webdriver.Ie(base_driver+"IEDriverServer.exe")
+            return driver
+        elif x == "ed":
+            driver = webdriver.Edge(executable_path=base_driver+"MicrosoftWebDriver.exe")
             return driver
         else:
-            if x == "fi":
-                driver = webdriver.Firefox(executable_path=root_driver+"geckodriver.exe")
-                return driver
-            else:
-                if x == "ie":
-                    driver = webdriver.Ie(executable_path=root_driver+"IEDriverServer.exe")
-                    return driver
-                else:
-                    if x == "ed":
-                        driver = webdriver.Edge(executable_path=root_driver+"MicrosoftWebDriver.exe")
-                        return driver
-                    else:
-                        return None
+            return None
+
     driver_web = drivers("ch")
--- util/functions.py
+++ util/functions.py
@@ -1,39 +1,24 @@
-import datetime
+import csv
+import os
+from datetime import datetime
+from time import sleep, strftime
 import random
 import string
 import psycopg2
 from util.config import ModelConfig
-from time import *
 
 
-def db_functions(code):
-    conn = None
-    valor=""
-    try:
-        conn = psycopg2.connect(ModelConfig.connection)
-        cur = conn.cursor()
-        exec(code)
-        try:
-            valor = cur.fetchmany()
-        except Exception as errorvalor:
-            print(errorvalor)
-        conn.commit()
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            return valor
-
+# Login
 def login(self):
     driver = self.driver
-    #login
-    driver.get(ModelConfig.url_login)
+    driver.get(ModelConfig.base_url+"/admin/login")
     driver.find_element_by_xpath('//*[@id="id_username"]').send_keys(ModelConfig.email)
     driver.find_element_by_xpath('//*[@id="id_password"]').send_keys(ModelConfig.password)
     driver.find_element_by_xpath('//*[@id="formLogin"]/button').click()
+    sleep(2)
 
+
+# Logout
 def logout(self):
     sleep(5)
     driver = self.driver
@@ -43,32 +28,73 @@ def logout(self):
     sleep(2)
 
 
-
-def screenshot(self,ruta):
+# Screenshot
+def screenshot(self, path):
     driver = self.driver
-    now = datetime.datetime.now()
-    hour = now.hour
-    min = now.min
-    second = now.second
-    today = datetime.date.today()
-    driver.save_screenshot(ModelConfig.base_screenshot+ruta+"%s-hora-%s-seg_%s.png" %(today, hour, second))
-    return ruta
+    now = datetime.now().strftime("%Y-%m-%d %H;%M;%S")
+    capture = ModelConfig.base_screenshot+path+" %s.png" % now
+    directory = os.path.dirname(capture)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    driver.save_screenshot(capture)
 
-def randoms(long, tipo):
-    dato = ""
-    if tipo == "letter":
-        letters = [chr(random.randint(97, 122)) for _ in range(long)]
-        dato = ''.join(letters)
-    else:
-        if tipo == "number":
-            numbers = [str(random.randint(0, 9)) for r in range(long)]
-            dato = ''.join(numbers)
-        else:
-            if tipo == "alpha":
-                alpha = [random.choice(string.ascii_letters + string.digits) for r in range(long)]
-                dato = ''.join(alpha)
-            else:
-                if tipo == "special":
-                    specials = [random.choice(string.punctuation) for r in range(long)]
-                    dato = ''.join(specials)
-    return dato
+
+# Randoms
+def randoms(long, _type):
+    value = ""
+    if _type == "letter":
+        letters = [chr(random.randint(97, 122)) for r in range(long)]
+        value = ''.join(letters)
+    elif _type == "number":
+        numbers = [str(random.randint(0, 9)) for r in range(long)]
+        value = ''.join(numbers)
+    elif _type == "alpha":
+        alpha = [random.choice(string.ascii_letters + string.digits) for r in range(long)]
+        value = ''.join(alpha)
+    elif _type == "special":
+        specials = [random.choice(string.punctuation) for r in range(long)]
+        value = ''.join(specials)
+    return value
+
+
+# DB functions
+# noinspection PyUnresolvedReferences
+def db_functions(code):
+    conn = None
+    result = ""
+    try:
+        conn = psycopg2.connect(ModelConfig.connection)
+        cur = conn.cursor()
+        exec(code)
+        try:
+            result = cur.fetchmany()
+        except Exception as errorFetch:
+            print(errorFetch)
+        conn.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+        return result
+
+
+# Read csv
+def read_csv(root):
+    csv_list = []
+    with open(root, newline='') as csv_file:
+        reader = csv.reader(csv_file)
+        title = reader.__next__()
+        for row in reader:
+            list_csv = {}
+            for column in range(len(title)):
+                list_csv[title[column]] = row[column]
+            csv_list.append(list_csv)
+    return csv_list
+
+
+# Delete file
+def delete_file(path):
+    if os.path.exists(path):
+        os.remove(path)
